2ca0dbea1c067be63faf633d4699a222460a7fb9
Rinat <Rinat.k95@mail.ru>
2022-07-14 22:14:30 +0500

    Task10

---
+++ DZ_2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+int a2=(a/10) % 10;
+int result = (a2);
+Console.WriteLine(result);
