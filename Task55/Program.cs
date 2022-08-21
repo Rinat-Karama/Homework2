8b61f400682bf4efd07e82ee08c06ef4f89b6772
Rinat <Rinat.k95@mail.ru>
2022-08-21 21:27:12 +0500

    Task55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -8,36 +8,37 @@ int m = Convert.ToInt32(Console.ReadLine());
 
 if (n == m)
 {
-int[,] matrix = new int[n, m];
+    int[,] matrix = new int[n, m];
 
-for (int i = 0; i < n; i++)
-{
-for (int j = 0; j < m; j++)
-{
-matrix[i, j] = new Random().Next(-100, 100);
-Console.Write(matrix[i, j] + " \t");
-}
-Console.WriteLine();
-}
-Console.WriteLine();
-for (int i = 0; i < n; i++)
-{
-for (int j = 0; j < m; j++)
-{
-if (j < i){
-int temp = matrix[i, j];
-matrix[i, j] = matrix[j, i];
-matrix[j, i] = temp;
-}
-}
-}
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = new Random().Next(-100, 100);
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (j < i)
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+            }
+        }
+    }
 
-for (int i = 0; i < n; i++)
-{
-for (int j = 0; j < m; j++)
-Console.Write(matrix[i, j] + " \t");
-Console.WriteLine();
-}
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
 }
 else
 Console.WriteLine("Увы... Это невозможно");
